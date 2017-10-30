d2e19f0ccc52e4055f2f67c77ae88ef7498047a8
rex yun <chgchg123@users.noreply.github.com>
2017-10-30 19:30:54 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ''
-    };
-  }
   render() {
     return (
-      <div>
-      </div>
+      <Contact/>
     );
   }
 }
